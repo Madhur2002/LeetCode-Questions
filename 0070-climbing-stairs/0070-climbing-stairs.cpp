d826ba753e42372868b04680c86654ef235bbbb6
Madhur Jain <93668517+Madhur2002@users.noreply.github.com>
2022-12-16 18:52:31 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int solve(int n , vector<int> &dp)
     {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        if(n==2) return 2;
+        if(n <= 2) return n;
         
         if(dp[n]!=-1) return dp[n];
         
