9fb06d3d7a186d539e0daa77e232350b95677161
Madhur Jain <93668517+Madhur2002@users.noreply.github.com>
2022-12-18 01:37:42 +0530

    Time: 7 ms (12.65%), Space: 7.8 MB (64.27%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,29 @@
 class Solution {
-private:
-    int helper2(vector<int>& dp2, vector<int>& nums, int index){
-        if(index > nums.size()-1) return 0;
-        if(index == nums.size()-1) return nums[nums.size() - 1];
-        if(dp2[index] != -1) return dp2[index];
-        int include = nums[index] + helper2(dp2, nums, index + 2);
-        int exclude = helper2(dp2, nums, index + 1);
-        int ans = max(include, exclude);
-        return dp2[index] = ans;
-    }
-    int helper1(vector<int>& dp1, vector<int>& nums, int index){
-        
-        if(index >= nums.size()-1) return 0;
-        // if(index == nums.size()) return nums[nums.size() - 1];
-        if(dp1[index] != -1) return dp1[index];
-        int include = nums[index] + helper1(dp1, nums, index + 2);
-        int exclude = helper1(dp1, nums, index + 1);
-        int ans = max(include, exclude);
-        return dp1[index] = ans;
-    }
 public:
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
         int n = nums.size();
-        vector<int>dp1(n+1, -1);
-        vector<int>dp2(n+1, -1);
-        int index = 0;
-        int ans1 = helper1(dp1, nums, index);
-        int ans2 = helper2(dp2, nums, index+1);
-        return max(ans1, ans2);
+        vector<int>dp1(n, 0);
+        vector<int>dp2(n, 0);
+        int ans1 = -1;
+        // dp1 -> not including the last element
+       //  dp2 -> not including the first element        
+        dp1[0] = nums[0];
+        for(int i=1; i<n-1;i++){
+            int include = nums[i];
+            if(i > 1) include += dp1[i-2];
+            int exclude = dp1[i-1] + 0;
+            dp1[i] = max(include, exclude);
+        }
+        
+        dp2[1] = nums[1];
+        for(int i=2; i<n; i++){
+            int include = nums[i];
+            if(i > 2) include += dp2[i-2];
+            int exclude = dp2[i-1];
+            dp2[i] = max(include, exclude);
+        }
+        return max(dp1[n-2], dp2[n-1]);
+        // return max(ans1, ans2);
     }
 };
