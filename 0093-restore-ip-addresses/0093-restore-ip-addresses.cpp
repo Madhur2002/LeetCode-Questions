db8cf45a2984843af9ba8fd1a12fff4f419c4b4d
Madhur Jain <93668517+Madhur2002@users.noreply.github.com>
2023-01-21 14:50:13 +0530

    Time: 6 ms (30.47%), Space: 6.8 MB (31.84%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    void solve(int ind, string nums, int dots, string ans, vector<string>& res, string& s){
+        if(dots == 3){
+            string r = s.substr(ind);
+            
+            if(r.size() == 0 || (r[0] == '0' && r.size() > 1)){
+                return;
+            }
+            
+            long temp = stol(r);
+            if(temp <= 255){
+                ans += r;
+                res.push_back(ans);
+            }
+            
+            return;
+        }
+        
+        if(ind >= n || nums[0] == '0') return;
+                
+        nums += s[ind];
+        long temp = stol(nums);
+        if(temp <= 255){
+            solve(ind+1, nums, dots, ans, res, s);
+            ans = ans + nums + ".";
+            solve(ind+1, "", dots+1, ans, res, s);
+        }else return;
+    }
+public:
+    int n;
+    vector<string> restoreIpAddresses(string s) {
+        n = s.size();
+        vector<string>res;
+        solve(0, "", 0, "", res, s);
+        return res;
+    }
+};
